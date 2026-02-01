db6dd38bd5b74631e9effd5dafe459114af11650
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2026-02-01 22:21:38 +0545

    dashboard and note complete ui/ux

---
+++ Air_write_system/src/components/liveWriting.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function LiveWriting() {
+  return (
+    <div>
+      <h1>LiveWriting</h1>
+    </div>
+  )
+}
---
+++ Air_write_system/src/components/modelStatus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+export default function ModelStatus() {
+  return (
+    <div>
+        <h1>ModelStatus</h1>
+    </div>
+  )
+}
