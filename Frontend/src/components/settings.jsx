b0138c44a5f6c313cf3257f262bfb1f7ac5e093b
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2026-02-07 20:46:56 +0545

    small chane of navigation

--- Frontend/src/components/settings.jsx
+++ Frontend/src/components/settings.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { useAuth } from '../api/authContex.jsx'
 
 export default function Settings() {
+  const {user,setUser} = useAuth();
+  console.log(user)
   return (
     <div>
-        <h1>Settings</h1>
+      setting
       
     </div>
   )
